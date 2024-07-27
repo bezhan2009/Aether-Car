98c7e536a1bbce727ff91c487d0026a86ad1ea18
bezhan2009 <karimovbezan0@gmail.com>
2024-07-27 21:07:12 +0300

    fix settings.py for deploy

---
+++ car_backend_Aether/set_env.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export DATABASE_URL="postgresql://test:Py6HoAxccayCEED72ARX2wLY1s2vayDr@dpg-cqhf8uiju9rs738kbgrg-a.singapore-postgres.render.com/test_naw3"
+export SECRET_KEY="django-insecure-1%xo6y&@j&yj1r03fa*!@8%p(#0mn)9=s_1$n+6i7%ktq2#8!p"
+export DEBUG=False
+export ALLOWED_HOSTS="*"
